7287fc6afe83c25f8e97b85bc89393105e806b2f
Gobinda Sarkar <gobindasarkar909@gmail.com>
2024-09-09 17:09:32 +0530

    working progress

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'https://com.diggiassist.app/',
+  appId: 'com.example.app',
   appName: 'diggiassist',
   webDir: 'out',
 };
